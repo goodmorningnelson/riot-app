be96290eb207c5b050c611c8a3ce54382857e382
goodmorningnelson <mtr.ruehr@gmail.com>
2016-12-13 17:07:31 -0500

    design on-going

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//need controller for things to work.
+angular.module('riot-app', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
+angular.module('riot-app').controller('appCtrl',['$scope','$window',function ($scope,$window) {
+    //angular media querry
+    console.log($window.width,$window);
+
+    $scope.playerSelectorModel = 'lcs';
+
+    $scope.skillSelectorModel = 'tool';
+}]);
